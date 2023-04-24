88119eba245c022b5c26916cf71c7aad4c50f45f
Connor S <constroh@gmail.com>
2023-04-24 17:33:18 -0500

    thought-routes updated

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -11,4 +11,8 @@ const {
 
 router.route("/").get(getAllThoughts).post(createThought);
 
-router.route("/:id").get(getThoughtById).delete(deleteThought);
+router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
