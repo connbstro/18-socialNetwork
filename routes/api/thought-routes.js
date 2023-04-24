89e6d3c2b279a73700a76579c514cd64b474a77f
Connor S <constroh@gmail.com>
2023-04-24 17:35:35 -0500

    syntax error update

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -16,3 +16,5 @@ router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought
 router.route("/:thoughtId/reactions").post(addReaction);
 
 router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
